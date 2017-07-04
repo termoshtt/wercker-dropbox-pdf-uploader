2b8b0d1016ac7c328dbd1f1b4c95a8f792c3de43
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-04 11:22:12 +0900

    Add run.sh and YAML

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+token=${DROPBOX_API_TOKEN}
+repo=${WERCKER_GIT_REPOSITORY:-WerckerCLI}
+branch=${WERCKER_GIT_BRANCH}
+sha=${WERCKER_GIT_COMMIT}
+
+echo "Repo   : ${repo}"
+echo "Branch : ${branch}"
+echo "SHA    : ${sha}"
+
+for pdf in *.pdf; do
+  curl -X POST https://content.dropboxapi.com/2/files/upload \
+      --header "Authorization: Bearer ${token}" \
+      --header "Dropbox-API-Arg: {\"path\": \"/${repo}/${branch}/${pdf}\",\"mode\": \"overwrite\"}" \
+      --header "Content-Type: application/octet-stream" \
+      --data-binary @${pdf}
+done
