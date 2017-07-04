7a86a77b4152173d63ec3532b7992325d174c257
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-04 11:37:13 +0900

    Add upload in _hash dir

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -eu
 
 token=${DROPBOX_API_TOKEN}
 repo=${WERCKER_GIT_REPOSITORY:-WerckerCLI}
@@ -13,6 +15,11 @@ for pdf in *.pdf; do
   curl -X POST https://content.dropboxapi.com/2/files/upload \
       --header "Authorization: Bearer ${token}" \
       --header "Dropbox-API-Arg: {\"path\": \"/${repo}/${branch}/${pdf}\",\"mode\": \"overwrite\"}" \
+      --header "Content-Type: application/octet-stream" \
+      --data-binary @${pdf}
+  curl -X POST https://content.dropboxapi.com/2/files/upload \
+      --header "Authorization: Bearer ${token}" \
+      --header "Dropbox-API-Arg: {\"path\": \"/${repo}/_hash/${sha}/${pdf}\",\"mode\": \"overwrite\"}" \
       --header "Content-Type: application/octet-stream" \
       --data-binary @${pdf}
 done
